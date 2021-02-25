9bfed93ebc245ae0d04029b7b885870be6eea7eb
tjuacalla123 <>
2021-02-25 10:55:39 -0800

    lab 8 up until rsvp

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
 function initMap() {
 	// add your code here
+  L.mapquest.key = '0POHVJghPUV7WDAYY05eASusyIxQnFOT';
+  var map = L.mapquest.map('map', {
+    center: [32.87860429567858, -117.23588958674019],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false 
+  });
+  L.marker([32.88, -117.236]).addTo(map);
 }
